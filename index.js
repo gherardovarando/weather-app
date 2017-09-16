b4ecba2e9c07ccb3f7a6056171ccd8c5f8e83249
gherardovarando <gherardo.varando@gmail.com>
2017-09-16 12:03:26 +0200

    update

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const Weather = require('node-weather');
 const Led = require('./led.js');
 
 let weather = new Weather({
-  key: ''
+    key: ''
 });
 
 let gw = new Led(18);
 let bw = new Led(23);
-weather.now('madrid', (info) => {
-    console.log(info.main);
-    if (info.main = 'Clear') {
-        gw.on();
-        bw.off();
-    } else {
-        gw.off();
-        bw.on();
-    }
-});
+
+
+gw.on('ready', () => {
+    weather.now('madrid', (info) => {
+        console.log(info.main);
+        if (info.main = 'Clear') {
+            gw.turnOn();
+            //bw.turnOff();
+        } else {
+            gw.turnOff();
+            //bw.turnOn();
+        }
+    });
+})
--- led.js
+++ led.js
@@ -1,33 +1,42 @@
 const gpio = require('rpi-gpio');
+const EventEmitter = require('events');
 
-class Led {
+
+class Led extends EventEmitter {
     constructor(ch, cl) {
         this.ch = ch;
+        this.ready = false;
         gpio.setup(ch, gpio.DIR_OUT, (err) => {
-          if (err) throw err;
+            if (err) throw err;
+            this.ready = true;
+            this.emit('ready');
         });
     }
 
-    on() {
+    turnOn() {
+        if (!this.ready) return;
         gpio.write(this.ch, true);
         gpio.status = true;
     }
 
-    off(){
-      gpio.write(this.ch, false);
-      this.status = false;
+    turnOff() {
+        if (!this.ready) return;
+        gpio.write(this.ch, false);
+        this.status = false;
     }
 
-    write(x){
-      gpio.write(this.ch, x);
-      this.status = x;
+    write(x) {
+        if (!this.ready) return;
+        gpio.write(this.ch, x);
+        this.status = x;
     }
 
-    toggle(){
-      this.write(!this.status);
+    toggle() {
+        if (!this.ready) return;
+        this.write(!this.status);
     }
 
 
 
 }
- module.exports = Led;
+module.exports = Led;
